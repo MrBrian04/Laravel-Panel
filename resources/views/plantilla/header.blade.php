1ac5a25cae0d03caa27fc832ceadfd77278d4bfd
MrBrian <bvergelgalvan@gmail.com>
2025-09-17 17:13:46 -0500

    Se agrego las rutas privadas, y se agrego el modulo de dashboard

--- resources/views/plantilla/header.blade.php
+++ resources/views/plantilla/header.blade.php
@@ -22,32 +22,32 @@
              </li>
              <!--end::Fullscreen Toggle-->
              <!--begin::User Menu Dropdown-->
-             <li class="nav-item dropdown user-menu">
-                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                     <img src="{{ asset('assets/img/user2-160x160.jpg') }}" class="user-image rounded-circle shadow"
-                         alt="User Image" />
-                     <span class="d-none d-md-inline">Alexander Pierce</span>
-                 </a>
+             @if(Auth::check())
+                <li class="nav-item dropdown user-menu">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                        <span class="d-none d-md-inline">{{Auth::user()->name}}</span>
+                    </a>
                  <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
                      <!--begin::User Image-->
                      <li class="user-header text-bg-primary">
-                         <img src="{{ asset('assets/img/user2-160x160.jpg') }}" class="rounded-circle shadow"
-                             alt="User Image" />
                          <p>
-                             Alexander Pierce - Web Developer
-                             <small>Member since Nov. 2023</small>
+                            {{Auth::user()->name}}
                          </p>
                      </li>
                      <!--end::User Image-->
 
                      <!--begin::Menu Footer-->
                      <li class="user-footer">
-                         <a href="#" class="btn btn-default btn-flat">Profile</a>
-                         <a href="#" class="btn btn-default btn-flat float-end">Sign out</a>
+                         <a href="#" onclick="document.getElementById('logout-form').submit();" class="btn btn-default btn-flat float-end">Cerrar  sesion</a>
                      </li>
                      <!--end::Menu Footer-->
                  </ul>
              </li>
+
+             <form action="{{route('logout')}}" id="logout-form" method="post" class="d-none">
+                @csrf
+             </form>
+             @endif
              <!--end::User Menu Dropdown-->
          </ul>
          <!--end::End Navbar Links-->
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\UserController; 
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('usuarios', UserController::class);
+Route::middleware(['auth'])->group(function(){
+    Route::resource('usuarios', UserController::class);
+    Route::patch('usuarios/{usuario}/toggle', [UserController::class, 'toggleStatus'])->name('usuarios.toggle');
+    Route::get('dashboard', function(){
+    return view('dashboard');
+})->name('dashboard');
 
-Route::patch('usuarios/{usuario}/toggle', [UserController::class, 'toggleStatus'])->name('usuarios.toggle');
+
+    Route::post('logout',function(){
+        Auth::logout();
+        return redirect('/login');
+    })->name('logout');
+});
+
+Route::middleware('guest')->group(function(){
+    Route::get('login', function(){ return view('autenticacion.login');})->name('login');
+    Route::post('login',[AuthController::class, 'login'])->name('login.post');
+});
