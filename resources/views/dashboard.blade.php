3197f29bf8fdbdb52682398332dfbc4262949509
MrBrian04 <bvergelgalvan@gmail.com>
2025-09-23 12:04:02 -0500

    Se actualizó y arreglo rutas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,38 +1,38 @@
 @extends('plantilla.app')
 @section('contenido')
-    <div class="app-content">
-        <!--begin::Container-->
-        <div class="container-fluid">
-            <!--begin::Row-->
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <h3 class="card-title">Dashboard</h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-
-                        </div>
-                        <!-- /.card-body -->
-                        <div class="card-footer clearfix">
-                           
-                        </div>
+<div class="app-content">
+    <!--begin::Container-->
+    <div class="container-fluid">
+        <!--begin::Row-->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h3 class="card-title">Dashboard</h3>
                     </div>
-                    <!-- /.card -->
+                    <!-- /.card-header -->
+                    <div class="card-body">
 
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer clearfix">
+
+                    </div>
                 </div>
-                <!-- /.col -->
+                <!-- /.card -->
 
             </div>
-            <!--end::Row-->
+            <!-- /.col -->
+
         </div>
-        <!--end::Container-->
+        <!--end::Row-->
     </div>
+    <!--end::Container-->
+</div>
 @endsection
 
 @push('scripts')
-    <script>
-        // document.getElementById('mnuDashboard').classList.add('active');
-    </script>
+<script>
+    // document.getElementById('mnuDashboard').classList.add('active');
+</script>
 @endpush
--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,19 @@ Route::get('/', function () {
 Route::middleware(['auth'])->group(function () {
     Route::resource('usuarios', UserController::class);
     Route::patch('usuarios/{usuario}/toggle', [UserController::class, 'toggleStatus'])->name('usuarios.toggle');
-    Route::get('dashboard', function () { return view('dashboard');})->name('dashboard');
+    Route::get('dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
     //segunda parte logout
-    Route::post('logout', function (){
-     Auth::logout();
-     return redirect('/login');
+    Route::post('logout', function () {
+        Auth::logout();
+        return redirect('/login');
     })->name('logout');
 });
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', function () { return view('autenticacion.login');})->name('login');
+    Route::get('login', function () {
+        return view('autenticacion.login');
+    })->name('login');
     Route::post('login', [AuthController::class, 'login'])->name('login.post');
 });
